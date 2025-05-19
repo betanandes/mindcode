1e278348564018718ef762483cb053f21e20e68b
betanandes <robertafernandes46@gmail.com>
2025-05-19 10:33:58 -0300

    tela inicial in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'main-screen',
+    loadChildren: () => import('./main-screen/main-screen.module').then( m => m.MainScreenPageModule)
+  },
 
 ];
 
---
+++ src/app/main-screen/main-screen.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-main-screen',
+  templateUrl: './main-screen.page.html',
+  styleUrls: ['./main-screen.page.scss'],
+})
+export class MainScreenPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
