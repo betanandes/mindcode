a32238899a4fa85eb9664201322b7c822499ab2a
unknown <gadelhayhuri@gmail.com>
2025-05-18 21:49:02 -0300

    start (integration and routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'start',
+    loadChildren: () => import('./start/start.module').then( m => m.StartPageModule)
+  },
 
 ];
 
