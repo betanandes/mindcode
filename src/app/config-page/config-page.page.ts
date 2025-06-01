dabb955d70ae64d678bf75e879373eea6426305d
Beatriz Gonçalves <167026758+Beatrizgon@users.noreply.github.com>
2025-06-01 15:48:57 -0300

    Ajustes e implementação de função ao Profile

--- src/app/config-page/config-page.page.ts
+++ src/app/config-page/config-page.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-config-page',
@@ -8,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigPagePage implements OnInit {
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  avatarUrl: string = 'https://ionicframework.com/docs/img/demos/avatar.svg';
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.avatarUrl = e.target.result; // Atualiza a imagem do avatar
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
 }
