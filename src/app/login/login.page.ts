dddc099df74cb2f3cf5b960e82b1588f63479e28
betanandes <robertafernandes46@gmail.com>
2025-06-01 22:08:23 -0300

    validação login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -14,37 +15,49 @@ export class LoginPage implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {}
 
   async login() {
     if (!this.email || !this.password) {
-      alert('Preencha todos os campos!');
+      this.presentToast('Preencha todos os campos!');
       return;
     }
 
     try {
       const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
       console.log('Usuário logado:', userCredential.user);
-      alert('Login realizado com sucesso!');
-      this.router.navigate(['/main-screen']); // ou a tela inicial do app
+      this.presentToast('Login realizado com sucesso!');
+      this.router.navigate(['/main-screen']);
     } catch (error: any) {
       console.error('Erro ao logar:', error);
       switch (error.code) {
         case 'auth/user-not-found':
-          alert('Usuário não encontrado. Verifique o e-mail.');
+          this.presentToast('Usuário não encontrado. Verifique o e-mail.');
           break;
         case 'auth/wrong-password':
-          alert('Senha incorreta.');
+          this.presentToast('Senha incorreta.');
           break;
         case 'auth/invalid-email':
-          alert('E-mail inválido.');
+          this.presentToast('E-mail inválido.');
           break;
         default:
-          alert('Erro ao logar. Tente novamente.');
+          this.presentToast('Erro ao logar. Tente novamente.');
       }
     }
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+      position: 'bottom',
+      cssClass: 'custom-toast'
+    });
+    toast.present();
+  }
 }
